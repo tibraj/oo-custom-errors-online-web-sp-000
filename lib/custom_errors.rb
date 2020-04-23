1ef121b4eea9982f43d9c9c7c37d60498e27d3f7
Taulant Ibraj <tibraj94@gmail.com>
2020-04-23 15:35:02 +0000

    Automatically backed up by Learn

--- lib/custom_errors.rb
+++ lib/custom_errors.rb
@@ -7,15 +7,15 @@ class Person
 
   def get_married(person)
     self.partner = person
-    person.partner = self
+    if person.class != Person
+      raise PartnerError
+    else
+      person.partner = self
   end
-
+  class PartnerError < StandardError
+  end
 end
 
 beyonce = Person.new("Beyonce")
 beyonce.get_married("Jay-Z")
 puts beyonce.name
-
-
-
-
