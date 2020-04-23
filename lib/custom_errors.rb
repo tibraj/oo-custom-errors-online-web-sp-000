4eda243bd76aef2c115b9b83a2b2573b1ad9f242
Taulant Ibraj <tibraj94@gmail.com>
2020-04-23 15:39:21 +0000

    Automatically backed up by Learn

--- lib/custom_errors.rb
+++ lib/custom_errors.rb
@@ -8,7 +8,11 @@ class Person
   def get_married(person)
     self.partner = person
     if person.class != Person
-      raise PartnerError #We need to tell our program to raise our brand new PartnerError when the argument passed into the #get_married method is not an instance of the Person class. We can do that with the raise keyword.
+      begin 
+       raise PartnerError #We need to tell our program to raise our brand new PartnerError when the argument passed into the #get_married method is not an instance of the Person class. We can do that with the raise keyword.
+      rescue PartnerError => error 
+        puts error.message 
+      end
     else
       person.partner = self
     end
