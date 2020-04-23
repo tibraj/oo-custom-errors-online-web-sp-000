a17537ccb63159a2be23c0fae05c97e801c8e0a4
Taulant Ibraj <tibraj94@gmail.com>
2020-04-23 15:40:48 +0000

    Automatically backed up by Learn

--- lib/custom_errors.rb
+++ lib/custom_errors.rb
@@ -8,10 +8,10 @@ class Person
   def get_married(person)
     self.partner = person
     if person.class != Person
-      begin 
+      begin #If the object passed into the method as an argument is not an instance of the Person class, we will begin our error handling. First, we raise our PartnerError, then we rescue our PartnerError. The rescue method creates an instance of the PartnerError class and puts out the result of calling message on that instance. not only is our custom error message printed out but the program continues to run and will execute the puts beyonce.name line
        raise PartnerError #We need to tell our program to raise our brand new PartnerError when the argument passed into the #get_married method is not an instance of the Person class. We can do that with the raise keyword.
-      rescue PartnerError => error 
-        puts error.message 
+      rescue PartnerError => error
+        puts error.message
       end
     else
       person.partner = self
